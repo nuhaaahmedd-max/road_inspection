6a43cf1bc7a5c1dac5d536efbd893a09a8900657
nuhaaahmedd-max <nuhaa.ahmedd@gmail.com>
2026-04-23 17:43:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ section[data-testid="stSidebar"] label {{
 /* ===== MULTISELECT BOX ===== */
 section[data-testid="stSidebar"] div[data-baseweb="select"] > div {{
     background-color: transparent !important;
-    border: 1.5x solid #FFD700 !important;
+    border: 2px solid #FFD700 !important;
     border-radius: 14px !important;
     padding: 8px !important;
 }}
