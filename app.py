b385e1cfd3904c922cbee4a95a6127b715730197
nuhaaahmedd-max <nuhaa.ahmedd@gmail.com>
2026-04-23 22:32:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ conf_threshold = st.sidebar.slider(
     step=0.05
 )
 
-  df_plot = df_plot[df_plot['Confidence'] >= confidence_min]
+ df_plot = df_plot[df_plot['Confidence'] >= confidence_min]
     csv = df_plot.to_csv(index=False).encode('utf-8')
     st.sidebar.download_button("📥 Download Report", data=csv, file_name='road_report.csv')
 
